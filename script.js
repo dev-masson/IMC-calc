ff5e4212e071e3861638a8fa16737ac96deaf060
Gabriel Masson <developermasson@gmail.com>
2023-07-07 19:21:39 -0300

    Restructuring data

--- script.js
+++ script.js
@@ -2,9 +2,22 @@ const form = document.querySelector('form');
 const inputWeight = document.querySelector('#weight');
 const inputHeight = document.querySelector('#height');
 
-const modalWrapper = document.querySelector('.modal-wrapper');
-const modalMesege = document.querySelector('.modal .title span');
-const modalButton = document.querySelector('.modal button.close');
+
+
+const Modal= {
+
+    wrapper: document.querySelector('.modal-wrapper'),
+    message: document.querySelector('.modal .title span'),
+    buttonClose: document.querySelector('.modal button.close'),
+
+    open() {
+        Modal.wrapper.classList.add('open');
+    },
+    close() {
+        Modal.wrapper.classList.remove('open');
+    }
+
+}
 
 
 form.onsubmit = (event) => {
@@ -17,12 +30,12 @@ form.onsubmit = (event) => {
     const result = IMC(weight, height);
     const message = `Seu IMC é ${result}`;
 
-    modalMesege.innerHTML = message;
-    modalWrapper.classList.add('open');
+    Modal.message.innerHTML = message;
+    Modal.open();
 }
 
-modalButton.onclick = () => {
-    modalWrapper.classList.remove('open');
+Modal.buttonClose.onclick = () => {
+    Modal.close();
 }
 
 function IMC(weight, height) {
