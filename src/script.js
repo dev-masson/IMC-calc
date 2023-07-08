64feb014cd1813d923a831d6375d5e741edfce76
Gabriel Masson <developermasson@gmail.com>
2023-07-07 22:23:51 -0300

    add new's messege modal

--- src/script.js
+++ src/script.js
@@ -32,7 +32,38 @@ form.onsubmit = (event) => {
 }
 
 function DisplayResultMessage(result){
-    const message = `Seu IMC é ${result}`;
+
+    let message;
+
+    if (result < 16) {
+        message = `Seu IMC é: ${result}<br>
+        Você está em Magreza Grau III`;
+    } else if (result >= 16 && result < 16.9) {
+        message = `Seu IMC é: ${result}<br>
+        Você está em Magreza Grau II`;
+    } else if (result >= 17 && result < 18.4) {
+        message = `Seu IMC é: ${result}<br>
+        Você está em Magreza Grau I`;
+    } else if (result >= 18.5 && result < 24.9) {
+        message = `Seu IMC é: ${result}<br>
+        Você está em Eutrofia`;
+    } else if (result >= 25 && result < 29.9) {
+        message = `Seu IMC é: ${result}<br>
+        Você está em Sobrepeso`;
+    } else if (result >= 30 && result < 34.9) {
+        message = `Seu IMC é: ${result}<br>
+        Você está em Obesidade Grau I`;
+    } else if (result >= 35 && result < 39.9) {
+        message = `Seu IMC é: ${result}<br>
+        Você está em Obesidade Grau II`;
+    } else {
+        message = `Seu IMC é: ${result}<br>
+        Você está em Obesidade Grau III`;
+    }
+    
+    console.log(message);
+    
+        
 
     Modal.message.innerHTML = message;
     Modal.open();
